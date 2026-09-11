faa36cd9340cffc58dde0a6a08e19644cb75e440
Johan Hoppe Rauer <johrau01@easv365.dk>
2026-09-11 10:44:10 +0200

    sdf

--- apps/REPLACE-WITH-DOMAIN-microservice/tests/obj/Debug/net10.0/tests.AssemblyInfo.cs
+++ apps/REPLACE-WITH-DOMAIN-microservice/tests/obj/Debug/net10.0/tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447ef3db26073947b8808d853639f99c4ba289ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041593014097157e32b635cc71fa7c43c509e618")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
